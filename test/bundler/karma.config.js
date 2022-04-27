ebcaaac861ba65e9844985384bba0745f9a9c9ee
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-04-27 15:15:45 -0400

    fmt

--- test/bundler/karma.config.js
+++ test/bundler/karma.config.js
@@ -16,18 +16,12 @@ const localLaunchers = {
 
 module.exports = function (config) {
   config.set({
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-jasmine',
-      'karma-polyfill',
-      'karma-typescript',
-    ],
+    plugins: ['karma-chrome-launcher', 'karma-jasmine', 'karma-polyfill', 'karma-typescript'],
     browsers: Object.keys(localLaunchers),
 
     singleRun: false, // set this to false to leave the browser open
 
-    frameworks: ['jasmine',
-      'karma-typescript', 'polyfill'],
+    frameworks: ['jasmine', 'karma-typescript', 'polyfill'],
 
     polyfill: ['Promise'],
     preprocessors: {
@@ -36,23 +30,21 @@ module.exports = function (config) {
     urlRoot: '/__karma__/',
     files: [
       // pattern: 'parcel-bundle-test/dist/*.{html,js}',
-    //   'parcel-bundle-test/dist/index.html',
-    // {
-    //     pattern: 'parcel-bundle-test/dist/*.{html,js}',
-    //     served: true,
-    //     nocache: true,
-    //     type:'module'
-    //   },
-    //   'parcel-bundle-test/parcel-bundle.spec.ts', // tells karma these are tests we need to serve & run
-      {pattern: 'vite-bundle-test/dist/index.html',
-        nocache: true,
-      included: false},
+      //   'parcel-bundle-test/dist/index.html',
+      // {
+      //     pattern: 'parcel-bundle-test/dist/*.{html,js}',
+      //     served: true,
+      //     nocache: true,
+      //     type:'module'
+      //   },
+      //   'parcel-bundle-test/parcel-bundle.spec.ts', // tells karma these are tests we need to serve & run
+      { pattern: 'vite-bundle-test/dist/index.html', nocache: true, included: false },
       {
         pattern: 'vite-bundle-test/dist/**/*.js',
-          included: false,
+        included: false,
 
-          nocache: true,
-          type: 'module',
+        nocache: true,
+        type: 'module',
       },
       'vite-bundle-test/vite-bundle.spec.ts', // tells karma these are tests we need to serve & run
       'util.ts',
@@ -68,13 +60,13 @@ module.exports = function (config) {
     ],
 
     karmaTypescriptConfig: {
-      exclude: ["./component-library"]
+      exclude: ['./component-library'],
     },
 
     // http://localhost:9876/__karma__/base/vite-bundle-test/dist/assets/index.dbcbef01.js
     //TODO - this is for vite only, and still doesn't pass
     proxies: {
-      '/assets/': `/base/vite-bundle-test/dist/assets/`
+      '/assets/': `/base/vite-bundle-test/dist/assets/`,
       // "/assets/": "/base/vite-bundle-test/dist/assets/**"
     },
 
