5a3c36fb3fd62e4a7b309e5b56d9770075fb8895
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-04-29 08:19:43 -0400

    util wip

--- test/bundler/karma-stencil-utils.ts
+++ test/bundler/karma-stencil-utils.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 
 /**
- *
+ * TODO
  * @returns
  */
 function waitFrame(): Promise<number> {
@@ -15,10 +15,10 @@ function waitFrame(): Promise<number> {
  */
 type DomTestUtilities = {
   /**
-   * Create and render an element containing the HTML at the provided url.
-   * @param url the location on disk containing the HTML to load
+   * Create and render the HTML at the provided url
+   * @param url a location on disk of a file containing the HTML to load
    * @param waitForStencilReadyMs the number of milliseconds to wait for the rendering pipeline to complete
-   * @returns
+   * @returns TODO
    */
   setupDom: (url: string, waitForStencilReadyMs?: number) => Promise<HTMLElement>;
   /**
@@ -34,6 +34,10 @@ type DomTestUtilities = {
  * @returns utilities to set up the DOM and tear it down within the test bed
  */
 export function setupDomTests(document: Document): DomTestUtilities {
+  /**
+   * All HTML will be rendered as a child of the test bed - get it (and create it if it doesn't exist) so that it is
+   * available for all future tests.
+   */
   let testBed = document.getElementById('test-app');
   if (!testBed) {
     testBed = document.createElement('div');
@@ -49,115 +53,114 @@ export function setupDomTests(document: Document): DomTestUtilities {
     app.className = 'test-spec';
     testBed.appendChild(app);
 
-    app.setAttribute('data-url', url);
     return renderTest(url, app, waitForStencilReadyMs);
   }
 
   /**
-   * Create web component for executing tests against
+   * Render HTML for executing tests against.
    * @param url the location on disk containing the HTML to load
-   * @param app
+   * @param app a parent HTML element to place test code in
    * @param waitForStencilReadyMs the number of milliseconds to wait for the rendering pipeline to complete
-   * @returns
+   * @returns TODO
    */
   function renderTest(url: string, app: HTMLElement, waitForStencilReadyMs: number): Promise<HTMLElement> {
     // 'base' is the directory that karma will serve all assets from
     url = path.join('base', url);
 
     return new Promise<HTMLElement>((resolve, reject) => {
-      try {
-        /**
-         *
-         * @returns
-         */
-        const stencilReady = (): Promise<any> => {
-          return allReady()
-            .then(() => waitFrame())
-            .then(() => allReady());
-        };
+      /**
+       *
+       * @param this
+       * @returns
+       */
+      const indexLoaded = function (this: XMLHttpRequest): void {
+        if (this.status !== 200) {
+          reject(`404: ${url}`);
+          return;
+        }
+        const frag = document.createDocumentFragment();
+        const elm = document.createElement('div');
+        elm.innerHTML = this.responseText;
+        frag.appendChild(elm);
+        app.innerHTML = elm.innerHTML;
 
-        /**
-         *
-         * @returns
-         */
-        const allReady = (): Promise<any[] | void> => {
-          const promises: Promise<any>[] = [];
-
-          /**
-           *
-           * @param promises
-           * @param elm
-           * @returns
-           */
-          const waitForDidLoad = (promises: Promise<any>[], elm: Element): void => {
-            if (elm != null && elm.nodeType === 1) {
-              for (let i = 0; i < elm.children.length; i++) {
-                const childElm = elm.children[i];
-                if (childElm.tagName.includes('-') && typeof (childElm as any).componentOnReady === 'function') {
-                  promises.push((childElm as any).componentOnReady());
-                }
-                waitForDidLoad(promises, childElm);
-              }
-            }
-          };
-
-          waitForDidLoad(promises, window.document.documentElement);
-
-          return Promise.all(promises).catch((e) => console.error(e));
-        };
-
-        /**
-         *
-         * @param this
-         * @returns
-         */
-        const indexLoaded = function (this: XMLHttpRequest): void {
-          if (this.status !== 200) {
-            reject(`404: ${url}`);
-            return;
+        const tmpScripts = app.querySelectorAll('script') as NodeListOf<HTMLScriptElement>;
+        for (let i = 0; i < tmpScripts.length; i++) {
+          const script = document.createElement('script') as HTMLScriptElement;
+          if (tmpScripts[i].src) {
+            script.src = tmpScripts[i].src;
           }
-          const frag = document.createDocumentFragment();
-          const elm = document.createElement('div');
-          elm.innerHTML = this.responseText;
-          frag.appendChild(elm);
-          app.innerHTML = elm.innerHTML;
-
-          const tmpScripts = app.querySelectorAll('script') as NodeListOf<HTMLScriptElement>;
-          for (let i = 0; i < tmpScripts.length; i++) {
-            const script = document.createElement('script') as HTMLScriptElement;
-            if (tmpScripts[i].src) {
-              script.src = tmpScripts[i].src;
-            }
-            if (tmpScripts[i].hasAttribute('nomodule')) {
-              script.setAttribute('nomodule', '');
-            }
-            if (tmpScripts[i].hasAttribute('type')) {
-              script.setAttribute('type', tmpScripts[i].getAttribute('type')!);
-            }
-            script.innerHTML = tmpScripts[i].innerHTML;
-
-            tmpScripts[i].parentNode!.insertBefore(script, tmpScripts[i]);
-            tmpScripts[i].parentNode!.removeChild(tmpScripts[i]);
+          if (tmpScripts[i].hasAttribute('nomodule')) {
+            script.setAttribute('nomodule', '');
           }
+          if (tmpScripts[i].hasAttribute('type')) {
+            script.setAttribute('type', tmpScripts[i].getAttribute('type')!);
+          }
+          script.innerHTML = tmpScripts[i].innerHTML;
 
-          elm.innerHTML = '';
+          tmpScripts[i].parentNode!.insertBefore(script, tmpScripts[i]);
+          tmpScripts[i].parentNode!.removeChild(tmpScripts[i]);
+        }
 
-          if (typeof waitForStencilReadyMs === 'number') {
-            setTimeout(() => {
+        elm.innerHTML = '';
+
+        if (typeof waitForStencilReadyMs === 'number') {
+          setTimeout(() => {
+            resolve(app);
+          }, waitForStencilReadyMs);
+        } else {
+          const appLoad = () => {
+            window.removeEventListener('appload', appLoad);
+            stencilReady().then(() => {
               resolve(app);
-            }, waitForStencilReadyMs);
-          } else {
-            const appLoad = () => {
-              window.removeEventListener('appload', appLoad);
-              stencilReady().then(() => {
-                resolve(app);
-              });
-            };
-            window.addEventListener('appload', appLoad);
+            });
+          };
+          window.addEventListener('appload', appLoad);
+        }
+      };
+
+      /**
+       *
+       * @returns
+       */
+      const stencilReady = (): Promise<any> => {
+        return allReady()
+          .then(() => waitFrame())
+          .then(() => allReady());
+      };
+
+      /**
+       *
+       * @returns
+       */
+      const allReady = (): Promise<any[] | void> => {
+        const promises: Promise<any>[] = [];
+
+        /**
+         *
+         * @param promises
+         * @param elm
+         * @returns
+         */
+        const waitForDidLoad = (promises: Promise<any>[], elm: Element): void => {
+          if (elm != null && elm.nodeType === 1) {
+            for (let i = 0; i < elm.children.length; i++) {
+              const childElm = elm.children[i];
+              if (childElm.tagName.includes('-') && typeof (childElm as any).componentOnReady === 'function') {
+                promises.push((childElm as any).componentOnReady());
+              }
+              waitForDidLoad(promises, childElm);
+            }
           }
         };
 
-        var oReq = new XMLHttpRequest();
+        waitForDidLoad(promises, window.document.documentElement);
+
+        return Promise.all(promises).catch((e) => console.error(e));
+      };
+
+      try {
+        const oReq = new XMLHttpRequest();
         oReq.addEventListener('load', indexLoaded);
         oReq.addEventListener('error', (err) => {
           console.error('error oReq.addEventListener', err);
